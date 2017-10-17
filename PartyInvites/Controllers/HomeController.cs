08306ce6d551b5779d4b5cc5834e7f2fce1b4bfc
digitopoi <hubo85@gmail.com>
2017-10-17 17:00:46 -0500

    Add home controller

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
